42ad73bb2426f5bd5371e836da9f5141f8448304
hikida444 <hikida44@gmail.com>
2026-08-27 17:56:31 +0300

    Initial Commit

---
+++ example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  example
+//
+//  Created by Gadèl Khantimirov on 27.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Gadèl Khantimirov on 27.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
